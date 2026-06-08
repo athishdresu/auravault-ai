27db5e155e31df3a94147256b069762c7b7aef2e
ATHISH M <athishdresu@gmail.com>
2026-06-08 13:19:41 +0530

    Remove unused ClerkProvider import

--- auravault/app/layout.tsx
+++ auravault/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import { ThemeProvider } from '@/components/theme-provider'
-import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
 
 const _geist = Geist({ subsets: ["latin"] });
